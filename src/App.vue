15bd81addcd51dcdfbe14c11137198667453152f
immy soun <immansounthon@gmail.com>
2023-07-23 18:19:03 +0100

    content
    added main content

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,4 @@
 </script>
 
 <template>
-  <body class="h-screen bg-black text-white">
-    <h1>Welcome.</h1>
-    <p>THIS WEBSITE IS UNDER CONSTRUCTION - i just changed my whole tech stack so it might be a while before content is released</p>
-  </body>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,54 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+function addStar()
+{
+    const geometry = new THREE.SphereGeometry(0.1, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread (100));
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(1000).fill().forEach(addStar);
+
+function moveCamera()
+{
+    const t = document.body.getBoundingClientRect().top;
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+
+function animate()
+{
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
